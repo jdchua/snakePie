a6684483a82e0db257958261d98ede1c1f67c934
jdchua <jeremiahdanielchua@gmail.com>
2019-08-29 02:18:08 +0000

    initial commit

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require("express"),
+    app     = express();
+    
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    res.render("snake");
+});
+
+app.listen(process.env.PORT, process.env.IP, function(){
+    console.log("snakeJS is running!");
+})
